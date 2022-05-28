c44c4ec9e7f42dd28af2af33f999732df64910f6
DECode.studio <decode.std@gmail.com>
2022-05-29 06:49:50 +0700

    chang db production

--- config/config.js
+++ config/config.js
@@ -16,11 +16,18 @@ module.exports = {
     "dialect": process.env.DB_DIALECT
   },
   "production": {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_NAME,
-    "host": process.env.DB_HOST,
+    "username": "juevowfamkhyos",
+    "password": "626b4ff4929bbc0bfcdff3e847e5d98770e85a56fbccd177f51f70b67334daa2",
+    "database": "d4q8iad6pchj95",
+    "host": "ec2-52-3-2-245.compute-1.amazonaws.com",
     "dialect": process.env.DB_DIALECT
   }
+  // "production": {
+  //   "username": process.env.DB_USERNAME,
+  //   "password": process.env.DB_PASSWORD,
+  //   "database": process.env.DB_NAME,
+  //   "host": process.env.DB_HOST,
+  //   "dialect": process.env.DB_DIALECT
+  // }
 }
 
